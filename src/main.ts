689b28c3c47a522d080583e52652650535b6b8e1
Necromayhem <aleksanderkuklev1488@gmail.com>
2025-05-03 02:00:06 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import { setupSwagger } from './utils/swagger.util';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Если нужны куки/авторизация
+  });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
